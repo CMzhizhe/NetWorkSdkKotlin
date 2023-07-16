a2fa57a77e4bd55342a5c5e13dfb166317b27180
gxx <745570899@qq.com>
2023-07-16 15:10:09 +0800

    未完成

--- app/src/main/java/com/gxx/networksdkkotlin/MainActivity.kt
+++ app/src/main/java/com/gxx/networksdkkotlin/MainActivity.kt
@@ -2,10 +2,30 @@ package com.gxx.networksdkkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.google.gson.GsonBuilder
+import com.gxx.neworklibrary.RequestHelper
+import com.gxx.neworklibrary.RequestResult
+import com.gxx.neworklibrary.RetrofitClient
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val gson = GsonBuilder().setPrettyPrinting().create()
+    private val apiService: ApiService by lazy { RetrofitClient.create() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lifecycleScope.launch{
+            launch {
+
+            }
+           
+        }
+
+
+
+
+
     }
 }
