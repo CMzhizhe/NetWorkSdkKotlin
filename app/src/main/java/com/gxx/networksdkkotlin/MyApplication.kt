3db6b6f4e3a0800d06a05366b5206839e981493d
gxx <745570899@qq.com>
2023-07-31 22:24:01 +0800

    1、移除一些不需要的

--- app/src/main/java/com/gxx/networksdkkotlin/MyApplication.kt
+++ app/src/main/java/com/gxx/networksdkkotlin/MyApplication.kt
@@ -3,19 +3,11 @@ package com.gxx.networksdkkotlin
 import android.app.Application
 import com.gxx.networksdkkotlin.network.WanAndroidMAFRequest
 import com.gxx.neworklibrary.OkHttpManager
-import com.gxx.testalibrary.TestANetWorkManager
-import com.gxx.testblibrary.TestBNetWorkManager
 
 class MyApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        OkHttpManager
-            .addAbsLaunchUrlReq(WanAndroidMAFRequest)
-            .create()
-        //构建相同的B域名
-        TestBNetWorkManager()
-        //拿到构建好的 Retrofit传递进去
-        TestANetWorkManager().init()
+        WanAndroidMAFRequest
     }
 }
